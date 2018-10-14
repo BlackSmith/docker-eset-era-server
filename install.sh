ea56f857e5c5930182973df4ec095ea2214f0ea5
Martin Korbel <mkorbel@redhat.com>
2018-10-14 10:48:19 +0200

    Fix URL

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 /opt/server-linux-x86_64.sh \
     --db-driver "MySQL ODBC Driver" \
     --db-hostname ${DB_HOSTNAME} \
+    --db-name ${DB_NAME} \
     --db-admin-username ${DB_ADMIN_USERNAME} \
     --db-admin-password ${DB_ADMIN_PASSWORD} \
     --db-user-username ${DB_USER_USERNAME} \
@@ -10,5 +11,6 @@
     --server-root-password ${ERA_ADMINISTRATOR_PASSWORD} \
     --cert-hostname ${ERA_CERT_HOSTNAME} \
     --locale ${ERA_LOCALE} \
+    --disable-imp-program \
     --skip-license
 
